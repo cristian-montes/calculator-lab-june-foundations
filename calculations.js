dac899f921025301c17d36668973a32d5a2b04b7
satyadvaya <satyadvaya@gmail.com>
2021-06-24 16:55:22 -0700

    failing test for multiply function

--- calculations.js
+++ calculations.js
@@ -5,3 +5,5 @@ export function add(num1, num2) {
 export function subtract(num1, num2) {
     return num1 - num2;
 }
+
+export function multiply(num1, num2) {}
--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,7 @@
 // IMPORT MODULES under test here:
 import { add } from '../calculations.js';
 import { subtract } from '../calculations.js';
+import { multiply } from '../calculations.js';
 
 const test = QUnit.test;
 
@@ -28,6 +29,21 @@ test('test subtracting', (expect) => {
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = subtract(5, 4); // use your function here
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+// name your test by what it is testing
+test('test multiplying', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 42;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = multiply(6, 7); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
