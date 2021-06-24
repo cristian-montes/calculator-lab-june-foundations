9f096e24a341772c145b6922d4c6de0ac06df8b1
satyadvaya <satyadvaya@gmail.com>
2021-06-24 16:02:59 -0700

    added subtract UI and button listener

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // import functions
 // import our add function
 import { add } from './calculations.js';
@@ -9,6 +8,10 @@ const firstAddInput = document.getElementById('first-add-input');
 const secondAddInput = document.getElementById('second-add-input');
 const addButton = document.getElementById('add-button');
 const sumDisplay = document.getElementById('sum-display');
+console.log(firstAddInput);
+console.log(secondAddInput);
+console.log(addButton);
+console.log(sumDisplay);
 
 // set event listeners 
   // get user input(s)
@@ -16,15 +19,28 @@ const sumDisplay = document.getElementById('sum-display');
   // update DOM to reflect new value(s)
 
   // add event listener to button click
-  addButton.addEventListener('click', ()=>{
+addButton.addEventListener('click', ()=>{
     console.log(firstAddInput.value);
-    // get the value #first-add-input -- convert to number
+// get the value #first-add-input -- convert to number
     const addend1 = Number(firstAddInput.value);
-    // get the value #second-add-input -- convert to number
+// get the value #second-add-input -- convert to number
     const addend2 = Number(secondAddInput.value);
-    // call add function with firstAddInput & secondAddInput
+// call add function with firstAddInput & secondAddInput
     const answer = add(addend1, addend2);
-    // replace the innertext of #sum-display with the result
+// replace the innertext of #sum-display with the result
     console.log(answer);
     sumDisplay.textContent = answer;
+});
+
+const firstSubtractInput = document.getElementById('first-subtract-input');
+const secondSubtractInput = document.getElementById('second-subtract-input');
+const subtractButton = document.getElementById('subtract-button');
+const differenceDisplay = document.getElementById('difference-display');
+console.log(firstSubtractInput);
+console.log(secondSubtractInput);
+console.log(subtractButton);
+console.log(differenceDisplay);
+
+subtractButton.addEventListener('click', ()=>{
+    console.log(firstSubtractInput.value);
 });
