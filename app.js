850fac0a18b5fbd771327d343f8d073953946703
satyadvaya <satyadvaya@gmail.com>
2021-07-02 14:16:23 -0700

    further edits to app.js

--- app.js
+++ app.js
@@ -8,14 +8,10 @@ import { modulo } from './calculations.js';
 
 // reference needed DOM elements
 // #first-add-input, #second-add-input, #add-button, #sum-display
-const firstAddInput = document.getElementById('first-add-input');
-const secondAddInput = document.getElementById('second-add-input');
-const addButton = document.getElementById('add-button');
-const sumDisplay = document.getElementById('sum-display');
-console.log(firstAddInput);
-console.log(secondAddInput);
-console.log(addButton);
-console.log(sumDisplay);
+const genInput1 = document.getElementById('gen-input1');
+const genInput2 = document.getElementById('gen-input2');
+const submitBtn = document.getElementById('submit-btn');
+const resultsDisplay = document.getElementById('results-display');
 
 // set event listeners 
   // get user input(s)
@@ -23,91 +19,91 @@ console.log(sumDisplay);
   // update DOM to reflect new value(s)
 
   // add event listener to button click
-addButton.addEventListener('click', () => {
-    console.log(firstAddInput.value);
+submitBtn.addEventListener('click', () => {
+    console.log(genInput1.value);
 // get the value #first-add-input -- convert to number
-    const addend1 = Number(firstAddInput.value);
+    const addend1 = Number(genInput1.value);
 // get the value #second-add-input -- convert to number
-    const addend2 = Number(secondAddInput.value);
+    const addend2 = Number(genInput2.value);
 // call add function with firstAddInput & secondAddInput
     const answer = add(addend1, addend2);
 // replace the innertext of #sum-display with the result
     console.log(answer);
-    sumDisplay.textContent = answer;
+    resultsDisplay.textContent = answer;
 });
 
-const firstSubtractInput = document.getElementById('first-subtract-input');
-const secondSubtractInput = document.getElementById('second-subtract-input');
-const subtractButton = document.getElementById('subtract-button');
-const differenceDisplay = document.getElementById('difference-display');
-console.log(firstSubtractInput);
-console.log(secondSubtractInput);
-console.log(subtractButton);
-console.log(differenceDisplay);
+// const firstSubtractInput = document.getElementById('first-subtract-input');
+// const secondSubtractInput = document.getElementById('second-subtract-input');
+// const subtractButton = document.getElementById('subtract-button');
+// const differenceDisplay = document.getElementById('difference-display');
+// console.log(firstSubtractInput);
+// console.log(secondSubtractInput);
+// console.log(subtractButton);
+// console.log(differenceDisplay);
 
-subtractButton.addEventListener('click', () => {
-    console.log(firstSubtractInput.value);
+// subtractButton.addEventListener('click', () => {
+//     console.log(firstSubtractInput.value);
 
-    const subtrahend1 = Number(firstSubtractInput.value);
-    const subtrahend2 = Number(secondSubtractInput.value);
-    const answer = subtract(subtrahend1, subtrahend2);
-    console.log(answer);
-    differenceDisplay.textContent = answer;
-});
+//     const subtrahend1 = Number(firstSubtractInput.value);
+//     const subtrahend2 = Number(secondSubtractInput.value);
+//     const answer = subtract(subtrahend1, subtrahend2);
+//     console.log(answer);
+//     differenceDisplay.textContent = answer;
+// });
 
-const firstMultiplyInput = document.getElementById('first-multiply-input');
-const secondMultiplyInput = document.getElementById('second-multiply-input');
-const multiplyButton = document.getElementById('multiply-button');
-const productDisplay = document.getElementById('product-display');
-console.log(firstMultiplyInput);
-console.log(secondMultiplyInput);
-console.log(multiplyButton);
-console.log(productDisplay);
+// const firstMultiplyInput = document.getElementById('first-multiply-input');
+// const secondMultiplyInput = document.getElementById('second-multiply-input');
+// const multiplyButton = document.getElementById('multiply-button');
+// const productDisplay = document.getElementById('product-display');
+// console.log(firstMultiplyInput);
+// console.log(secondMultiplyInput);
+// console.log(multiplyButton);
+// console.log(productDisplay);
 
-multiplyButton.addEventListener('click', () => {
-    console.log(firstMultiplyInput.value);
+// multiplyButton.addEventListener('click', () => {
+//     console.log(firstMultiplyInput.value);
 
-    const factor1 = Number(firstMultiplyInput.value);
-    const factor2 = Number(secondMultiplyInput.value);
-    const answer = multiply(factor1, factor2);
-    console.log(answer);
-    productDisplay.textContent = answer;
-});
+//     const factor1 = Number(firstMultiplyInput.value);
+//     const factor2 = Number(secondMultiplyInput.value);
+//     const answer = multiply(factor1, factor2);
+//     console.log(answer);
+//     productDisplay.textContent = answer;
+// });
 
-const firstDivideInput = document.getElementById('first-divide-input');
-const secondDivideInput = document.getElementById('second-divide-input');
-const divideButton = document.getElementById('divide-button');
-const quotientDisplay = document.getElementById('quotient-display');
-console.log(firstDivideInput);
-console.log(secondDivideInput);
-console.log(divideButton);
-console.log(quotientDisplay);
+// const firstDivideInput = document.getElementById('first-divide-input');
+// const secondDivideInput = document.getElementById('second-divide-input');
+// const divideButton = document.getElementById('divide-button');
+// const quotientDisplay = document.getElementById('quotient-display');
+// console.log(firstDivideInput);
+// console.log(secondDivideInput);
+// console.log(divideButton);
+// console.log(quotientDisplay);
 
-divideButton.addEventListener('click', () => {
-    console.log(firstDivideInput.value);
+// divideButton.addEventListener('click', () => {
+//     console.log(firstDivideInput.value);
 
-    const divisor1 = Number(firstDivideInput.value);
-    const divisor2 = Number(secondDivideInput.value);
-    const answer = divide(divisor1, divisor2);
-    console.log(answer);
-    quotientDisplay.textContent = answer;
-});
+//     const divisor1 = Number(firstDivideInput.value);
+//     const divisor2 = Number(secondDivideInput.value);
+//     const answer = divide(divisor1, divisor2);
+//     console.log(answer);
+//     quotientDisplay.textContent = answer;
+// });
 
-const firstModuloInput = document.getElementById('first-modulo-input');
-const secondModuloInput = document.getElementById('second-modulo-input');
-const moduloButton = document.getElementById('modulo-button');
-const remainderDisplay = document.getElementById('remainder-display');
-console.log(firstModuloInput);
-console.log(secondModuloInput);
-console.log(moduloButton);
-console.log(remainderDisplay);
+// const firstModuloInput = document.getElementById('first-modulo-input');
+// const secondModuloInput = document.getElementById('second-modulo-input');
+// const moduloButton = document.getElementById('modulo-button');
+// const remainderDisplay = document.getElementById('remainder-display');
+// console.log(firstModuloInput);
+// console.log(secondModuloInput);
+// console.log(moduloButton);
+// console.log(remainderDisplay);
 
-moduloButton.addEventListener('click', () => {
-    console.log(firstModuloInput.value);
+// moduloButton.addEventListener('click', () => {
+//     console.log(firstModuloInput.value);
 
-    const operand1 = Number(firstModuloInput.value);
-    const operand2 = Number(secondModuloInput.value);
-    const answer = modulo(operand1, operand2);
-    console.log(answer);
-    remainderDisplay.textContent = answer;
-});
+//     const operand1 = Number(firstModuloInput.value);
+//     const operand2 = Number(secondModuloInput.value);
+//     const answer = modulo(operand1, operand2);
+//     console.log(answer);
+//     remainderDisplay.textContent = answer;
+// });
